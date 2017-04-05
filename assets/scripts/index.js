13a5873f3bdfc1b36fed508ebd2bdd9d17eda998
ryaong792 <ryan.ongchoa@libertymutual.com>
2017-04-05 13:07:49 -0400

    Added Sign-up /sign-on /sign out /change password created new folder called auth. created 3 new files: api, events, ui (.js) edit HTML for buttons/forms area on top of the page as a place holder before nav bar is put in. updated index.js and typgraphy: Put background on header as pink to see form field while testing formating.  Will change.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,6 +13,13 @@ $(() => {
 // use require without a reference to ensure a file is bundled
 require('./example')
 
+const authEvents = require('./auth/events.js')
+
+// On document ready
+$(() => {
+  authEvents.addHandlers()
+})
+
 const player1 = 'X'
 const player2 = 'O'
 let currentTurn = player1
@@ -23,4 +30,11 @@ let currentTurn = player1
 $('.square').on('click', function (e) {
   $(this).html(currentTurn = currentTurn === player1 ? player2 : player1)
   $(this).unbind('click')
+  //if { Need a winning combo}
 })
+
+//let winningArray = {
+  //[1,2,3], [4,5,6], [7,8,9], [1,4,7], [1,5,9], [2,4,8], [3,5,7], [3,6,9]
+
+  // If any of these patters of board spaces have all X's or all O's somebody won!
+  //wins: [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]],
