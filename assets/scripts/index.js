35bfde84b87bf3bd117c6a407738bc92b847dd44
ryaong792 <ryan.ongchoa@libertymutual.com>
2017-04-05 09:46:14 -0400

    Added JS/JQ script for X and O, need to add files to script area for API. JS/JQ added to index is for pointer X and O/current turn and unbound button via Jquery.  Need to find winratio etc.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,3 +12,15 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 require('./example')
+
+const player1 = 'X'
+const player2 = 'O'
+let currentTurn = player1
+// javascript .on click feature.  $('ID or class').on('click', Dosomething)
+// condition ? expression1 : expression2 short cut for if
+// conditon will need to be true/false.  Didn't work when i had no var
+// TAKE YOU! YOU TUBE!
+$('.square').on('click', function (e) {
+  $(this).html(currentTurn = currentTurn === player1 ? player2 : player1)
+  $(this).unbind('click')
+})
