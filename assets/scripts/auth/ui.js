58da44c5b29043fbc141c813db08df5e16e63389
ryaong792 <ryan.ongchoa@libertymutual.com>
2017-04-15 13:47:25 -0400

    Added the Alert messages Added in UI the alert message for Sign-up: Success: Show success message and 5 sec then message is removed Failure: shows fail message and 5 sec then message is removed Sign-in: Success: Show success message and 5 sec then message is removed Failure: shows fail message and 5 sec then message is removed sign-out: Success: Show success message and 5 sec then message is removed Change password: Success: Show success message and 5 sec then message is removed Failure: shows fail message and 5 sec then message is removed
    Also added another hide feature, stating that if you change your
    password successfully to log in again.  When successfully change
    password the sign-up and sign-in prompt will be showed.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -4,9 +4,19 @@ const store = require('../store')
 const game = require('../game/events.js')
 
 const signUpSuccess = (data) => {
+  $('.success').show().text('Account has been created successfully.  Please sign-in to play!')
+  setTimeout(function () {
+    $('.success').fadeOut().empty()
+  }, 5000)
+  return false
 }
 
 const signUpFailure = () => {
+  $('.failure').show().text('Account creation failed.  Email may already be in used or passwords did not match.  Please try again!')
+  setTimeout(function () {
+    $('.failure').fadeOut().text('')
+  }, 7000)
+  return false
 }
 
 const signInSuccess = (response) => {
@@ -17,9 +27,19 @@ const signInSuccess = (response) => {
   $('.board').show()
   $('#sign-in').hide()
   $('#sign-up').hide()
+  $('.success').show().text('Sign-in Successful! Enjoy your game!')
+  setTimeout(function () {
+    $('.success').fadeOut().empty()
+  }, 5000)
+  return false
 }
 
 const signInFailure = () => {
+  $('.failure').show().text('Email or passowrd is incorrect! Please try again!')
+  setTimeout(function () {
+    $('.failure').fadeOut().text('')
+  }, 6000)
+  return false
 }
 
 const signOutSuccess = () => {
@@ -30,6 +50,10 @@ const signOutSuccess = () => {
   $('.stats').text('')
   $('#change-password').hide()
   $('.board').hide()
+  $('.success').show().text('You have successfully signed-out! Please sign-in to play again!')
+  setTimeout(function () {
+    $('.success').fadeOut().empty()
+  }, 5000)
 }
 
 const signOutFailure = () => {
@@ -37,9 +61,25 @@ const signOutFailure = () => {
 
 const changePasswordSuccess = () => {
   store.user = null
+  $('#sign-up').show()
+  $('#sign-in').show()
+  $('#sign-out').hide()
+  $('.stats').text('')
+  $('#change-password').hide()
+  $('.board').hide()
+  $('.success').show().text('Password has been change successfully! Please Sign in with your new password to play!')
+  setTimeout(function () {
+    $('.success').fadeOut().empty()
+  }, 6000)
+  return false
 }
 
 const changePasswordFailure = () => {
+  $('.failure').show().text('Failed to change password! Old Password did not match! Please try again!')
+  setTimeout(function () {
+    $('.failure').fadeOut().text('')
+  }, 6000)
+  return false
 }
 
 module.exports = {
